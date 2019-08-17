17b3c4a06f04e3e42aff62642ffda2825f6edc20
Brandon Allison <brandona8128@outlook.com>
2019-08-17 16:48:58 -0400

    mvp

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+export const GET_SMURFS = 'GET_SMURFS';
+export const ERROR = 'ERROR';
+export const ADD_SMURF = 'ADD_SMURF';
+
+export const getSmurfs = () => dispatch => {
+    axios
+        .get('http://localhost:3333/smurfs')
+        .then(response => {
+            console.log(response.data)
+            dispatch({ type: GET_SMURFS, payload: response.data })
+        })
+        .catch(err => {
+            dispatch({ type: ERROR, payload: err.errorMessage })
+        })
+}
+
+export const addSmurf = newSmurf => dispatch => {
+        axios
+            .post('http://localhost:3333/smurfs', newSmurf)
+            .then(response => {
+                console.log(response.data)
+                dispatch({type: ADD_SMURF, payload: response.data})
+            })
+            .catch(err => {
+                dispatch({ type: ERROR, payload: err.errorMessage })
+            })
+    }
